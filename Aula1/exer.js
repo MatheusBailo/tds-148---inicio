1027dad97ba75d9b40730f9c323b608907a26a42
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2025-02-18 15:28:22 -0300

    mudança pessoal

---
+++ Aula1/exer.js
@@ -0,0 +1,27 @@
+// EXERCICIOS variaveis
+
+//Declara uma variavel com a sua idade
+const idade = 17
+//Declarar uma variavel com a sua comida preferida e atribuir um valor
+let comidaPreferida = "pizza"
+// Declarar uma variavel e nao dar valor pra ela, sendo undefined
+let carro
+console.log(carro)
+// Declarar 5 variaveis diferentes sem valores
+var moto,caminhão,foguete,salada,celular
+// Declarar uma variavel contendo uma string
+let name = "Matheus"
+// Declare uma varivel contendo um numero dentro de uma string
+let numero
+numero = "10"
+// Declare uma variavel com seu nome, e outra com sobrenome, e concatene os dois, depois faça um template string dos 2
+var nome, sobrenome
+nome = "Matheus"
+sobrenome = "Serrute"
+console.log(`meu nome é ${nome} ${sobrenome}.`)
+console.log("Meu nome é " + nome + " e meu sobrenome é " + sobrenome)
+// Coloque a seguinte frase em uma variavel: It´s time
+var frase = "its time"
+// verifique o tipo de todas as variaveis criadas
+console.log(typeof idade, typeof carro,)
+
---
+++ Aula1/script.js
@@ -0,0 +1,41 @@
+let nome = "matheus"
+//string
+
+const idade = 17
+//number
+
+
+var temCabeloComprido = "outra"
+var temCabeloComprido = "mais"
+//booleon
+
+//console.log(typoof nome, nome)
+//console.log(typoof idade, idade)
+//console.log(typoof temCabeloComprido, temCabeloComprido)
+
+//console.log(typoof nome, typoof idade, typoof temCabeloComprido)
+
+temCabeloComprido = "mais uma"
+console.log(temCabeloComprido)
+
+
+var preco = 25
+var quantidade = 5
+var total = (preco * quantidade) - 10
+
+//console.log(total)
+
+//concatenação
+
+var sobrenome, cidade, ano
+
+sobrenome = "serrute"
+cidade = "Curitiba"
+ano = 2025
+
+console.log(nome, sobrenome, cidade)
+console.log("meu nome é " 
+    + nome + " " + 
+    ", moro em " 
+    + cidade + ".")
+console.log(`meu nome é ${nome} ${sobrenome}, eu moro em ${curitiba}.`)
